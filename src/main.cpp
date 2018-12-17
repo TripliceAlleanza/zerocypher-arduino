c6b82b00a99e82d89b56af222ebbe22ca2ae2795
Edoardo <Edoardo@email.com>
2018-12-17 11:42:23 +0100

    Beeper, movimento funzionanti, cripta cesare non funzionante se i >= z

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,57 @@
 #include <ArduinoJson.h>
 
 #define SERVO_PIN 10
+#define STEP 7.2f
 
 Servo myServo;
 
 String waitForString();
+void writeLetter(char);
+void writeString(String);
+String ceasarEncrypt(String,int);
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(SERVO_PIN, OUTPUT);
   myServo.attach(SERVO_PIN);
-  myServo.write(0);
+  myServo.write(180);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // while(!Serial.available());
-  // String json = waitForString();
-  
-  // Serial.println(json);
-  // myServo.write(0);
+  String value = waitForString();
+  Serial.println(value.c_str());
+  writeString(ceasarEncrypt(value, 1));
+}
 
-  // for(int i = 700;i<=2300;i+=64) {
-  //   myServo.writeMicroseconds(i);
-  //   Serial.println(i);
-  //   delay(500);
-  // }
-  myServo.writeMicroseconds(700);
-  delay(2000);
-  myServo.writeMicroseconds(2600);
-  delay(2000);
+String ceasarEncrypt(String s, int key) {
+  String newString = String();
+  for(char c : s) {
+    char nc = c+key;
+    newString.concat(nc);
+  }
+  Serial.println(newString);
+}
+
+void writeString(String str) {
+  for(int i = 0; i<str.length(); ++i) {
+    if((int)str[i] == 13) continue;
+    writeLetter(str[i]);
+    tone(3, 1000, 200);
+    delay(1000);
+  }
+}
+
+void writeLetter(char c) {
+  int value = (int)c;
+  int writeValue = value - 97;
+  myServo.write(180 - STEP*writeValue);
 }
 
 String waitForString() {
   String str = "";
-  while(Serial.available() > 0) {
-    str += String(Serial.read());
-    if(str=="\0") break;
-  }
+  while(Serial.available() == 0){;}
+  str = Serial.readStringUntil('\n');
+  Serial.println("String letta:" + str);
   return str;
 }
