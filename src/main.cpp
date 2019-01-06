3cda18882ea17a87dd956fb32ca2cda5f376d678
Edoardo <edoardo396@yahoo.it>
2019-01-06 19:40:21 +0100

    JSON Parsing, ceasar encrypt i/o

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,29 @@
 #define SERVO_PIN 10
 #define STEP 7.2f
 
+#pragma region typedef
+struct InPacket {
+  int id;
+  String message;
+  int key;
+  bool mode;
+  String algorithm; // TODO not used
+};
+
+struct OutPacket {
+  int id;
+  String status;
+};
+
+#pragma endregion
+
 Servo myServo;
 
 String waitForString();
 void writeLetter(char);
 void writeString(String);
 String ceasarEncrypt(String,int);
+String ceasarDecrypt(String,int);
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,18 +40,54 @@ void setup() {
 void loop() {
   String value = waitForString();
   Serial.println(value.c_str());
-  writeString(ceasarEncrypt(value, 1));
+  InPacket packet = processJSON(value);
+  
+  auto string = getWriteString(packet);
+  writeString(string);
+  
+  OutPacket out {packet.id, "OK"};
+  Serial.println(serializeOutput(out));
 }
 
+#pragma region processing
+InPacket processJSON(String json) {
+  StaticJsonBuffer<300> buffer;
+  JsonObject& root = buffer.parseObject(json.c_str());
+  
+  InPacket packet {root["id"], root["msg"], root["key"], root["mode"], root["algorithm"]};
+  return packet; 
+}
+
+String serializeOutput(OutPacket packet) {
+  char buffer[100];
+  sprintf(buffer, "{\"id\"=%d,\"status\"=%s", packet.id, packet.status);
+  return String(buffer);
+}
+
+String getWriteString(InPacket packet) {
+  if(packet.algorithm == "ceasar") {
+    return packet.mode ? ceasarEncrypt(packet.message, packet.key) : ceasarDecrypt(packet.message, packet.key);
+  }
+}
+#pragma endregion
+
+#pragma region criptatura
 String ceasarEncrypt(String s, int key) {
   String newString = String();
   for(char c : s) {
-    char nc = c+key;
+    auto nc = c+key;
     newString.concat(nc);
   }
   Serial.println(newString);
 }
 
+String ceasarDecrypt(String e, int key) {
+  return "notimpl";
+}
+
+#pragma endregion
+
+#pragma region i-o
 void writeString(String str) {
   for(int i = 0; i<str.length(); ++i) {
     if((int)str[i] == 13) continue;
@@ -57,3 +110,4 @@ String waitForString() {
   Serial.println("String letta:" + str);
   return str;
 }
+#pragma endregion
